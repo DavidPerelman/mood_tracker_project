b8c82a836ab4f9c35b703124df103780576b4e4c
DavidPerelman <53565536+DavidPerelman@users.noreply.github.com>
2025-04-28 15:46:30 +0300

    API into files with APIRouter

--- api/main.py
+++ api/main.py
@@ -1,26 +1,6 @@
 from fastapi import FastAPI
-from api.models.mood import UserMood, UserMoodIn
+from api.routers.mood import router as mood_router
 
 app = FastAPI()
 
-
-mood_table = {}
-
-
-@app.post("/moods", response_model=UserMood)
-async def create_mood(mood: UserMoodIn):
-    data = mood.model_dump()
-    last_record_id = len(mood_table)
-    new_mood = {**data, "id": last_record_id}
-    mood_table[last_record_id] = new_mood
-    return new_mood
-
-
-@app.get("/moods", response_model=list[UserMood])
-async def get_moods():
-    return list(mood_table.values())
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World!"}
+app.include_router(mood_router)
---
+++ api/routers/mood.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from api.models.mood import UserMood, UserMoodIn
+
+router = APIRouter()
+
+
+mood_table = {}
+
+
+@router.post("/moods", response_model=UserMood)
+async def create_mood(mood: UserMoodIn):
+    data = mood.model_dump()
+    last_record_id = len(mood_table)
+    new_mood = {**data, "id": last_record_id}
+    mood_table[last_record_id] = new_mood
+    return new_mood
+
+
+@router.get("/moods", response_model=list[UserMood])
+async def get_moods():
+    return list(mood_table.values())
+
+
+@router.get("/")
+async def root():
+    return {"message": "Hello World!"}
